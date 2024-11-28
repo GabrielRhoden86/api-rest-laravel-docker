c96cb149b222e36d5bd7934b9c2fb24de4ebe546
Gabriel Rhoden <gabrielrhodden@gmail.com>
2024-11-28 13:35:23 -0300

    correção

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-# Espera o banco de dados estar acessível
 echo "Aguardando o banco de dados..."
-while ! nc -z db 3306; do
+while ! nc -z mysql-db 3306; do
   sleep 1
 done
 
-# Executa as migrações e seeds
+echo "Banco de dados acessível, executando migrações e seeds"
+
 php artisan migrate --force
 php artisan db:seed --force
 
-# Inicia o Apache
+echo "Migrações e seeds concluídas, iniciando Apache"
+
 apache2-foreground
